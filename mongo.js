bb743dad0c98f68933c2bc1aba6dcb1af7b7e10f
julianxrenner <julesman18@gmail.com>
2026-01-28 06:28:36 -0500

    prettier format

--- mongo.js
+++ mongo.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if(process.argv.length < 3){
-    console.log('Dont forget to include your password when running');
-    process.exit(1)
+if (process.argv.length < 3) {
+  console.log("Dont forget to include your password when running");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url = `mongodb+srv://renner_db_phonebook:${password}@cluster0.5yaf6vd.mongodb.net/phonebookApp?appName=Cluster0`
+const url = `mongodb+srv://renner_db_phonebook:${password}@cluster0.5yaf6vd.mongodb.net/phonebookApp?appName=Cluster0`;
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false);
 
-mongoose.connect(url, { family: 4 })
+mongoose.connect(url, { family: 4 });
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-  })
+  name: String,
+  number: String,
+});
 
-const Contact = mongoose.model('Contact', contactSchema)
+const Contact = mongoose.model("Contact", contactSchema);
 
 const contact = new Contact({
-    name: process.argv[3],
-    number: process.argv[4]
-  })
+  name: process.argv[3],
+  number: process.argv[4],
+});
 
-  contact.save().then(result => {
-    console.log('contact saved!')
-    mongoose.connection.close()
-  })
+contact.save().then((result) => {
+  console.log("contact saved!");
+  mongoose.connection.close();
+});
