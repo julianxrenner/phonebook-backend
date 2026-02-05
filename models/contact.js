6ff5b96757097731d9a7a0b1450db6b1bf920d57
julianxrenner <julesman18@gmail.com>
2026-02-05 05:59:50 -0500

    added regex to confirm phone number

--- models/contact.js
+++ models/contact.js
@@ -17,7 +17,16 @@ mongoose
 
 const contactSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
 });
 
 contactSchema.set("toJSON", {
